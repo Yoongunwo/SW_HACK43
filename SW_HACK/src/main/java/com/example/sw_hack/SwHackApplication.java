80f4f605fb823ff838d7d2af80aad80ca5e98bfa
Yoongunwo <glove2275@pusan.ac.kr>
2023-06-28 23:40:41 +0900

    레포 생성

---
+++ SW_HACK/src/main/java/com/example/sw_hack/SwHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.sw_hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwHackApplication.class, args);
+    }
+
+}
